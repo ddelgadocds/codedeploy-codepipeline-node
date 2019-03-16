abbdefa859dcbee7c183c24ba5ab3f2571a3e1f3
Damian Delgado <ddelgado@codigodelsur.com>
2019-03-16 17:12:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require('http')
   .createServer((req, res) => {
     console.log(`incoming url: ${req.url} and incoming method: ${req.method}`)
     res.writeHeader(200,{'Content-Type': 'text/html'})
-    res.write('<p>Buenos dias Montevideo</p>')
-    res.write('<p>This is Saturday. Bla blah blah</p>')
+    res.write('<p>BuenAS tardes URUGUAY</p>')
+    res.write('<p>This is Saturday.XXXXXXXX :D</p>')
     res.end('<h1>Hello World from amazing Node University course AWS Intermediate</h1>')
   })
   .listen(port, (error)=>{
